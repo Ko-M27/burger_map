1c5b34db3512e247ef5a0b8d04d471967027d66d
Ko-M27 <m.kodai9527@gmail.com>
2020-08-31 12:39:17 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to:'shops#index'
+  root 'shops#index'
   resources :shops
 end
